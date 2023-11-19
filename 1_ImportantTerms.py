ed869a3971744906c52682890e5d853b99ee5ae0
Harshikha <85279763+Har8899@users.noreply.github.com>
2023-11-19 16:44:53 +0530

    Add file via upload

---
+++ 1_ImportantTerms.py
@@ -0,0 +1,55 @@
+'''Some Important terms
+------------------------
+------------------------
+
+1. Identifier
+--------------
+An identinfier is a name having a few letters, numbers and special
+character _ (underscore).
+it should always start with a non- numeric character.
+it is used to identify a variable, function, symbolic constant, class etc.
+
+Python is a case sensitive programming language.
+eg: d is not = D
+
+KEYWORDS OR RESERVERD WORDS
+-----------------------------
+
+Python language uses the following keywords which are available to users to use them as identifiers.
+
+1.False     2.await     3.else      4.import        5.pass
+6.None      7.break     8.except    9.in            10.raise
+11.True     12.class    13.finally  14.is           15.return
+16.and      17.continue 18.for      19.lambda       20.try
+21.as       22.def      23.from     24.nonlocal     25.while
+26.assert   27.del      28.global   29.not          30.with
+31.async    32.elif     33.if       34.or           35.yield
+
+2. Constants
+--------------
+A constant is an identifier whose value cannot be changed throughout the execution of a program whereas the variable value keeps on changing.
+There are no constants in Puthon, the way they exist in C and java.
+In Python, It is not possible to define constant whose value can not be changed.
+In Python, Constants ate usually defined on a module level and written in all capital letters with undrscores separating words but remember its value can be changed.
+
+3. Variable
+-------------
+A Python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing. Every value in Python has a datatype. Different data types in Python are Numbers, List, Tuple, Strings, Dictionary, etc.
+
+Rules---
+1.Every variable name should start with alphabets or underscore(_).
+2.No spaces are allowed in variable declaration.
+3.Except underscore(_) no other special symbol are allowed in the middle of the variable declaration
+4.A variable is written with a combination of letters, numbers and special character _ (underscore)
+
+
+
+
+
+
+
+
+
+
+
+
